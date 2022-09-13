4d18e13d157d769a8229bd853a94372c6e5c5efa
daniloemboaba <danilo.emboaba@hotmail.com>
2022-09-13 11:20:45 -0300

    Inserção do background logo e imagens dos games

--- www/web/src/App.tsx
+++ www/web/src/App.tsx
@@ -1,9 +1,37 @@
 import './styles/main.css';
 
+import logoImg from './assets/logo.svg';
+<assets />
+
 function App() {
     return (
-       <div className="w-8 h-8 bg-black sm:via-violet-500">
-       </div>
+       <div  className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+       <img src={logoImg}/>
+
+       <h1 className="text-6xl text-white font-black"> Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.</h1>
+            
+            
+            <div className="grid grid-cols-6 gap-6 mt-16">
+                <a href="">
+                    <img src="/game-1.png" alt=""/>
+                    </a> 
+                       <a href="">
+                    <img src="/game-2.png" alt=""/>
+                    </a> 
+                           <a href="">
+                    <img src="/game-3.png" alt=""/>
+                    </a> 
+                                  <a href="">
+                    <img src="/game-4.png" alt=""/>
+                    </a> 
+                                 <a href="">
+                    <img src="/game-5.png" alt=""/>
+                    </a>
+                                 <a href="">
+                    <img src="/game-6.png" alt=""/>
+                    </a> 
+            </div>
+        </div>
     )
 }
 
