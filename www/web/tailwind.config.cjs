92b2e73e9986cbd2ce5c4e43e132c908a4856cf4
daniloemboaba <danilo.emboaba@hotmail.com>
2022-09-13 23:13:29 -0300

    Box, gradient e texto de anuncio dos glabels games

--- www/web/tailwind.config.cjs
+++ www/web/tailwind.config.cjs
@@ -9,7 +9,8 @@ module.exports = {
 
       backgroundImage:{
         galaxy: "URL('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 15.08%, #43E7AD 35.94%, #E1D55D 80.57%);'
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 15.08%, #43E7AD 35.94%, #E1D55D 80.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
