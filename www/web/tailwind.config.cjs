81bb0c0da0693b2aa6c7a17cc16cb2cf7964fd0f
daniloemboaba <danilo.emboaba@hotmail.com>
2022-09-14 21:51:51 -0300

    finalização da landpage inicial

--- www/web/tailwind.config.cjs
+++ www/web/tailwind.config.cjs
@@ -5,16 +5,16 @@ module.exports = {
     './index.html',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+
     extend: {
 
       backgroundImage:{
         galaxy: "URL('/background-galaxy.png')",
-<<<<<<< HEAD
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 15.08%, #43E7AD 35.94%, #E1D55D 80.57%)',
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
-=======
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 15.08%, #43E7AD 35.94%, #E1D55D 80.57%);'
->>>>>>> 03c235fab82cf621510ab5673f24d81a3abab7b9
       },
     },
   },
